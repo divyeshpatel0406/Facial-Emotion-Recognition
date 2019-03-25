ce4d8e8736c59b654ea7c64a3fc92d87e5480dbf
Geronimo Lopez <lopez.geronimo213@gmail.com>
2019-03-25 18:20:39 -0500

    Updated face detector to also detect facial landmarks and color code
    Note the provided shape predictor 'shape_predictor_68_face_landmarks.dat' is supposedly not allowed for commercial use. Still, the code works as a proof of concept while another predictor is found or trained.

--- Viola Jones Face Detection/face_detector_folder.py
+++ Viola Jones Face Detection/face_detector_folder.py
@@ -7,16 +7,24 @@ The output will draw these rectangles on
 the original, unaltered color image. Will also output the names of result images
 where no face was detected or bad lighting is present.
 
+Has been updated to also detect facial landmarks and color different features accordingly.
+
 Note the input folder and output folder paths must be altered to suit the environment.
+Note the predictor path for the landmark detection must be altered to suit the environment.
+
 """
 
 import glob
 import time
 import cv2
+import numpy as np
+import dlib
 
 #PATHS FOR INPUT AND OUTPUT FOLDERS ARE REQUIRED
 IMAGES = [cv2.imread(file) for file in glob.glob('faces/*jpg')] #Input path
 PATH = 'faces/results/' #Output path
+#PATH FOR THE PREDICTOR FILE FOR DETECTING LANDMARKS
+PREDICTOR_PATH = 'shape_predictor_68_face_landmarks.dat'
 
 #Using local copy of cascade to ensure it doesn't change or "update" during development
 FACE_CASCADE = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -60,14 +68,14 @@ for img in IMAGES: #Go through each file in the folder
             print("Bad lighting " + str(i))
         #Draw a rectangle around the faces detected in the image
         for (x, y, w, h) in faces2:
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            img2 = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         j += 1
         CLAHE_TIME += time.time() - start #Add runtime to running total
 
     else:
         #Draw a rectangle around the faces detected in the image
         for (x, y, w, h) in faces:
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            img2 = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
             #This is the Region of Interest for our image (The area of the face)
             roi_gray = gray[y:y+h, x:x+w]
@@ -75,8 +83,42 @@ for img in IMAGES: #Go through each file in the folder
         k += 1
 
     #Write the altered image to the results folder
-    cv2.imwrite(PATH + 'pic %d.jpg'%i, img)
+    #cv2.imwrite(PATH + 'pic %d.jpg'%i, img)
     i += 1
+
+
+    #The predictor that will be used to detect landmarks
+    predictor = dlib.shape_predictor(PREDICTOR_PATH)
+
+
+    #Convert the openCV rect into a dlib rectangle
+    dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+
+    def get_color(index):
+        '''
+        Returns an RGB color value to be used to color the various landmarks detected on the face.
+        '''
+        #Colors that will be used to color the landmarks detected
+        colors = [(0, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
+
+        if 0 <= index <= 17:
+            return colors[0]
+        if 18 <= index <= 27:
+            return colors[1]
+        if 28 <= index <= 36:
+            return colors[2]
+        if 37 <= index <= 48:
+            return colors[3]
+
+        return colors[4]
+
+    #Detect landmarks, convert to numpy matrix, and iterate through the landmarks
+    detected_landmarks = predictor(img, dlib_rect).parts()
+    landmarks = np.matrix([[p.x, p.y] for p in detected_landmarks])
+    for idx, point in enumerate(landmarks, start=1):
+        pos = (point[0, 0], point[0, 1])
+        cv2.circle(img, pos, 3, color=get_color(idx)) #Draw a circle and color it accordingly
+    cv2.imwrite(PATH + 'pic %d.jpg'%i, img)
 
 #Hold average runtimes
 NORMAL_TIME_AVG = 0.0
