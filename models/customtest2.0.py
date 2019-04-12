1499b8291419bc2e816668f93a071657ba498de6
Geronimo Lopez <lopez.geronimo213@gmail.com>
2019-04-12 09:38:55 -0500

    Updated to show emotion and percent confidence in each class

--- models/customtest2.0.py
+++ models/customtest2.0.py
@@ -9,12 +9,12 @@ import numpy as np
 import cv2
 
 #loading the model
-json_file = open('fer.json', 'r')
+json_file = open('fer_70.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("fer.h5")
+loaded_model.load_weights("fer_70.hdf5")
 print("Loaded model from disk")
 
 #setting image resizing parameters
@@ -25,21 +25,24 @@ y=None
 labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 #loading image
-full_size_image = cv2.imread("test_images/Angry2.jpg")
+full_size_image = cv2.imread("test_images/Happy1.jpg")
 print("Image Loaded")
 gray=cv2.cvtColor(full_size_image,cv2.COLOR_RGB2GRAY)
 
 ##Haar Cascade Code  goes here.
 cropped_img = np.expand_dims(np.expand_dims(cv2.resize(gray, (48, 48)), -1), 0)
-final_image = loaded_model.predict(cropped_img)
+final_image = loaded_model.predict_proba(cropped_img)
+
 
 #Print Emotions in order of Confidence
-emotion_confidence = final_image.argsort().argsort()
-emotion_confidence = emotion_confidence.ravel()
+# emotion_confidence = final_image.argsort().argsort()
+# print(emotion_confidence)
+# emotion_confidence = emotion_confidence.ravel()
+# print(emotion_confidence)
 #print(emotion_confidence)
-temp = emotion_confidence.tolist();
-for index in range(len(emotion_confidence)):
-	print("Emotion: " + labels[temp[index]])
+print("Emotion: "+labels[int(np.argmax(final_image))])
+for index in range(6):
+	print(labels[index] + ': {0:.4f}%'.format(final_image[0][index] * 100))
 cv2.imshow('Emotion', full_size_image)
 cv2.waitKey()
 
