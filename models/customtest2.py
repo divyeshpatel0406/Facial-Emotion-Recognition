23c75c2756c21cff71b462686a683966adb24559
Geronimo Lopez <lopez.geronimo213@gmail.com>
2019-04-20 17:50:25 -0500

    Updated customtest2 preprocessing
    Apply CLAHE on the image and detect faces. Then, only the region with the face is passed to the model. Yielding better results in small "selfie" test set.

--- models/customtest2.py
+++ models/customtest2.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 import cv2
 
-#loading the model
+# loading the model
 json_file = open('fer_70.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
@@ -17,38 +17,50 @@ loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights("fer_70.hdf5")
 print("Loaded model from disk")
 
-#setting image resizing parameters
+# setting image resizing parameters
 WIDTH = 48
 HEIGHT = 48
 x=None
 y=None
 labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
-#loading image
-full_size_image = cv2.imread("test_images/Happy1.jpg")
+# loading image
+full_size_image = cv2.imread("test_images/Surprised1.jpg")
 print("Image Loaded")
-gray=cv2.cvtColor(full_size_image,cv2.COLOR_RGB2GRAY)
+gray = cv2.cvtColor(full_size_image, cv2.COLOR_RGB2GRAY)
+CLAHE_2 = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+gray = CLAHE_2.apply(gray)
 
-##Haar Cascade Code  goes here.
-cropped_img = np.expand_dims(np.expand_dims(cv2.resize(gray, (48, 48)), -1), 0)
+# Using local copy of cascade to ensure it doesn't change or "update" during development
+FACE_CASCADE = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faces = FACE_CASCADE.detectMultiScale(gray, 1.09, 3) #Attempt to find faces in the image
+largest_region = (0, 0, 0, 0)
+print("Detected {} face(s)".format(len(faces)))
+for (x, y, w, h) in faces:
+    if w > largest_region[2] and h > largest_region[3]:
+        largest_region = x, y, w, h
+x, y, w, h = largest_region
+gray_roi = gray[y:y+h, x:x+w]
+
+cropped_img = np.expand_dims(np.expand_dims(cv2.resize(gray_roi, (48, 48)), -1), 0)
 final_image = loaded_model.predict_proba(cropped_img)
 
 
-#Print Emotions in order of Confidence
+# Print Emotions in order of Confidence
 # emotion_confidence = final_image.argsort().argsort()
 # print(emotion_confidence)
 # emotion_confidence = emotion_confidence.ravel()
 # print(emotion_confidence)
-#print(emotion_confidence)
+# print(emotion_confidence)
 print("Emotion: "+labels[int(np.argmax(final_image))])
-for index in range(6):
-	print(labels[index] + ': {0:.4f}%'.format(final_image[0][index] * 100))
-cv2.imshow('Emotion', full_size_image)
+for index in range(7):
+    print(labels[index] + ': {0:.4f}%'.format(final_image[0][index] * 100))
+cv2.imshow('Emotion', gray_roi)
 cv2.waitKey()
 
-#Save image to folder
+# Save image to folder
 emotion_max_label = labels[int(np.argmax(final_image))]
 savepath = "correct_images/" + emotion_max_label + "_correct.jpg"
-cv2.imwrite(savepath, full_size_image)
-#savepath = "wrong_images/" + emotion_max_label +"_wrong.jpg"
-#cv2.imwrite(savepath, full_size_image)
+cv2.imwrite(savepath, gray_roi)
+# savepath = "wrong_images/" + emotion_max_label +"_wrong.jpg"
+# cv2.imwrite(savepath, full_size_image)
